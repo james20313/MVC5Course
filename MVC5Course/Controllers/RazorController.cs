1b0dcac9c09343cc4adf7844e32b2b0f1e5a1af3
JamesChen <james20313@gmail.com>
2018-07-15 11:20:14 +0800

    將 Bootstrap_Metro_Dashboard 版面變成 Layout 頁面

---
+++ MVC5Course/Controllers/RazorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class RazorController : Controller
+    {
+        // GET: Razor
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Page1()
+        {
+
+            return View();
+        }
+
+        public ActionResult DashBoad()
+        {
+
+            return View();
+        }
+    }
+}
